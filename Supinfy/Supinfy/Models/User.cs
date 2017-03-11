ee0cf6d4b14c5f97a2211a7899548e05d6eb85a8
Dramelac <203697@supinfo.com>
2017-03-11 19:14:04 +0100

    Add entity framwork + localdb

---
+++ Supinfy/Supinfy/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Supinfy.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Nickname { get; set; }
+        
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+    }
+}
