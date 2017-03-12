8f77be815874158f1716e91ea23cfc671616b95f
Dramelac <203697@supinfo.com>
2017-03-12 19:26:28 +0100

    Create controlle, index view, redirect on login, create DAO

---
+++ Supinfy/Supinfy/Controllers/PlaylistController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Supinfy.Utils;
+
+namespace Supinfy.Controllers
+{
+    public class PlaylistController : Controller
+    {
+        // GET: Playlist
+        public ActionResult Index()
+        {
+            if (Session[SessionKey.UserId] == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            return View();
+        }
+    }
+}
---
+++ Supinfy/Supinfy/DAL/PlaylistDAO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Supinfy.Models;
+
+namespace Supinfy.DAL
+{
+    public static class PlaylistDAO
+    {
+        public static List<Playlist> GetAllPlaylistsFromUserId(Guid userId)
+        {
+            return DataContext.Instance.Playlist.Where(p => p.OwnerId == userId).ToList();
+        }
+
+        public static void CreatePlaylist(Playlist playlist)
+        {
+            DataContext.Instance.Playlist.Add(playlist);
+            DataContext.Instance.SaveChanges();
+        }
+    }
+}
