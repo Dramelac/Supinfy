f024e01879e5245ceec589f05ba9996d327a2b52
djjoman21 <djjomail2@gmail.com>
2017-03-20 23:19:22 +0100

    add some CSS and make player working on .music click

--- Supinfy/Supinfy/Scripts/Site.js
+++ Supinfy/Supinfy/Scripts/Site.js
@@ -6,3 +6,18 @@
         data: 'id=' + trackId
     });
 }
+$(".music").on("click", function() {
+    var container = document.getElementById("player_container");
+    var player = document.getElementById("player");
+    var img = document.getElementById("artwork");
+    var music_name_container = document.getElementById("player_music_name");
+    img.setAttribute("src", this.dataset.artwork);
+    var artist = this.dataset.artist;
+    var title = this.dataset.title;
+    music_name_container.innerHTML = artist + " - " + title;
+    player.setAttribute("src", this.dataset.url)
+    container.style.display = "block";
+    player.play();
+    counter(this.dataset.id)
+});
+
