86146e1771f38bc65c88138331f85399fafd891e
djjoman21 <djjomail2@gmail.com>
2017-03-21 17:40:47 +0100

    Add music to BDD if not exist when add to playlist

--- Supinfy/Supinfy/Controllers/PlaylistController.cs
+++ Supinfy/Supinfy/Controllers/PlaylistController.cs
@@ -8,6 +8,10 @@ using Supinfy.DAL;
 using Supinfy.Models;
 using Supinfy.Utils;
 using Supinfy.ViewModel;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+using Supinfy.Models;
 
 namespace Supinfy.Controllers
 {
@@ -109,6 +113,41 @@ namespace Supinfy.Controllers
                 var playlist = PlaylistDAO.GetPlaylist(playlistId);
                 if (playlist != null && playlist.OwnerId == (Guid)Session[SessionKey.UserId])
                 {
+                    Music music = MusicDAO.GetMusicByTrackId(trackId);
+                    if (music == null)
+                    {
+                        string url = "https://api.jamendo.com/v3.0/tracks?client_id=ab5d1fe6&id=" + trackId.ToString();
+                        HttpWebRequest r = (HttpWebRequest)WebRequest.Create(url);
+                        r.Method = "GET";
+                        HttpWebResponse res = (HttpWebResponse)r.GetResponse();
+                        Stream sr = res.GetResponseStream();
+                        StreamReader sre = new StreamReader(sr);
+                        string s = sre.ReadToEnd();
+                        MusicSearchVM data = JsonConvert.DeserializeObject<MusicSearchVM>(s);
+                        if (data.Headers.code == 0)
+                        {
+                            if (data.Headers.results_count > 0)
+                            {
+                                MusicAPIVM result = data.Results[0];
+                                Music newMusic = new Music();
+                                newMusic.Artist = result.artist_name;
+                                newMusic.Title = result.name;
+                                newMusic.TrackId = result.id;
+                                newMusic.TrackUrl = result.audio;
+                                newMusic.ArtworkUrl = result.image;
+                                MusicDAO.AddMusic(newMusic);
+                            }
+                            else
+                            {
+                                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+                            }
+                        }
+                        else
+                        {
+                            return new HttpStatusCodeResult(HttpStatusCode.InternalServerError);
+                        }
+                    }
+
                     if (PlaylistDAO.AddMusicToPlaylist(playlistId, trackId))
                     {
                         return new HttpStatusCodeResult(HttpStatusCode.OK);
